ca084fb5b62cb3495ec5a6856283b17a52c9394c
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-05-21 22:14:14 +0900

    set to netstandard2.0

--- src/ExpressionCache.cs
+++ src/ExpressionCache.cs
@@ -1,20 +1,11 @@
-﻿/*
-* Copyright © 2021 MyNihongo
-*/
+﻿namespace MyNihongo.Expressions;
 
-using System;
-using System.Collections.Concurrent;
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("MyNihongo.Expressions.Tests")]
-namespace MyNihongo.Expressions
+internal static class ExpressionCache
 {
-	internal static class ExpressionCache
-	{
-		internal static readonly ConcurrentDictionary<Tuple<Type, string>, Lazy<Delegate>>
-			PropertyGetters = new(),
-			PropertySetters = new();
+	internal static readonly ConcurrentDictionary<Tuple<Type, string>, Lazy<Delegate>>
+		PropertyGetters = new(),
+		PropertySetters = new();
 
-		internal static readonly ConcurrentDictionary<Tuple<Type, string>, Lazy<Delegate>>
-			Invoke = new();
-	}
+	internal static readonly ConcurrentDictionary<Tuple<Type, string>, Lazy<Delegate>>
+		Invoke = new();
 }
--- src/PropertyOf.cs
+++ src/PropertyOf.cs
@@ -1,108 +1,100 @@
-﻿/*
-* Copyright © 2021 MyNihongo
-*/
+﻿namespace MyNihongo.Expressions;
 
-using System;
-using System.Linq.Expressions;
-
-namespace MyNihongo.Expressions
+/// <summary>
+/// Utility class for getting and settings properties
+/// </summary>
+public static class PropertyOf
 {
-	/// <summary>
-	/// Utility class for getting and settings properties
-	/// </summary>
-	public static class PropertyOf
+	public static object? Get(object source, string propertyName) =>
+		Get<object?>(source, propertyName);
+
+	public static T? Get<T>(object source, string propertyName)
 	{
-		public static object? Get(object source, string propertyName) =>
-			Get<object?>(source, propertyName);
-
-		public static T? Get<T>(object source, string propertyName)
+		var key = new Tuple<Type, string>(source.GetType(), propertyName);
+		var @delegate = ExpressionCache.PropertyGetters.GetOrAdd(key, static x =>
 		{
-			var key = new Tuple<Type, string>(source.GetType(), propertyName);
-			var @delegate = ExpressionCache.PropertyGetters.GetOrAdd(key, static x =>
+			return new Lazy<Delegate>(() =>
 			{
-				return new Lazy<Delegate>(() =>
-				{
-					var param = Expression.Parameter(x.Item1);
-					var prop = Expression.Property(param, x.Item2);
+				var param = Expression.Parameter(x.Item1);
+				var prop = Expression.Property(param, x.Item2);
 
-					return Expression.Lambda(prop, param).Compile();
-				});
-			}).Value;
+				return Expression.Lambda(prop, param).Compile();
+			});
+		}).Value;
 
-			return (T?)@delegate.DynamicInvoke(source);
-		}
-
-		public static void Set(object source, string propertyName, object? value)
-		{
-			if (string.IsNullOrEmpty(propertyName))
-				return;
-
-			var key = new Tuple<Type, string>(source.GetType(), propertyName);
-			var @delegate = ExpressionCache.PropertySetters.GetOrAdd(key, static x =>
-			{
-				return new Lazy<Delegate>(() =>
-				{
-					var param = Expression.Parameter(x.Item1);
-					var prop = Expression.Property(param, x.Item2);
-					var valueParam = Expression.Parameter(typeof(object));
-					var convertedValueParam = Expression.Convert(valueParam, prop.Type);
-					var assign = Expression.Assign(prop, convertedValueParam);
-
-					return Expression.Lambda(assign, param, valueParam).Compile();
-				});
-			}).Value;
-
-			@delegate.DynamicInvoke(source, value);
-		}
-
-		public static void Set<T>(object source, string propertyName, T value)
-		{
-			if (string.IsNullOrEmpty(propertyName))
-				return;
-
-			var key = new Tuple<Type, string>(source.GetType(), propertyName);
-			var @delegate = ExpressionCache.PropertySetters.GetOrAdd(key, static x =>
-			{
-				return new Lazy<Delegate>(() =>
-				{
-					var param = Expression.Parameter(x.Item1);
-					var prop = Expression.Property(param, x.Item2);
-					var valueParam = Expression.Parameter(typeof(T));
-					var assign = Expression.Assign(prop, valueParam);
-
-					return Expression.Lambda(assign, param, valueParam).Compile();
-				});
-			}).Value;
-
-			@delegate.DynamicInvoke(source, value);
-		}
+		return (T?)@delegate.DynamicInvoke(source);
 	}
 
-	/// <summary>
-	/// Utility class for getting and settings properties
-	/// </summary>
-	public static class PropertyOf<T>
+	public static void Set(object source, string propertyName, object? value)
 	{
-		public static TDestination Get<TDestination>(T obj, string propertyName) =>
-			Cache<TDestination>.Get(propertyName)(obj);
+		if (string.IsNullOrEmpty(propertyName))
+			return;
 
-		private static class Cache<TDestination>
+		var key = new Tuple<Type, string>(source.GetType(), propertyName);
+		var @delegate = ExpressionCache.PropertySetters.GetOrAdd(key, static x =>
 		{
-			public static Func<T, TDestination> Get(string propertyName)
+			return new Lazy<Delegate>(() =>
 			{
-				var key = new Tuple<Type, string>(typeof(T), propertyName);
+				var param = Expression.Parameter(x.Item1);
+				var prop = Expression.Property(param, x.Item2);
+				var valueParam = Expression.Parameter(typeof(object));
+				var convertedValueParam = Expression.Convert(valueParam, prop.Type);
+				var assign = Expression.Assign(prop, convertedValueParam);
 
-				return (Func<T, TDestination>)ExpressionCache.PropertyGetters.GetOrAdd(key, static x =>
+				return Expression.Lambda(assign, param, valueParam).Compile();
+			});
+		}).Value;
+
+		@delegate.DynamicInvoke(source, value);
+	}
+
+	public static void Set<T>(object source, string propertyName, T value)
+	{
+		if (string.IsNullOrEmpty(propertyName))
+			return;
+
+		var key = new Tuple<Type, string>(source.GetType(), propertyName);
+		var @delegate = ExpressionCache.PropertySetters.GetOrAdd(key, static x =>
+		{
+			return new Lazy<Delegate>(() =>
+			{
+				var param = Expression.Parameter(x.Item1);
+				var prop = Expression.Property(param, x.Item2);
+				var valueParam = Expression.Parameter(typeof(T));
+				var assign = Expression.Assign(prop, valueParam);
+
+				return Expression.Lambda(assign, param, valueParam).Compile();
+			});
+		}).Value;
+
+		@delegate.DynamicInvoke(source, value);
+	}
+}
+
+/// <summary>
+/// Utility class for getting and settings properties
+/// </summary>
+public static class PropertyOf<T>
+{
+	public static TDestination Get<TDestination>(T obj, string propertyName) =>
+		Cache<TDestination>.Get(propertyName)(obj);
+
+	private static class Cache<TDestination>
+	{
+		public static Func<T, TDestination> Get(string propertyName)
+		{
+			var key = new Tuple<Type, string>(typeof(T), propertyName);
+
+			return (Func<T, TDestination>)ExpressionCache.PropertyGetters.GetOrAdd(key, static x =>
+			{
+				return new Lazy<Delegate>(() =>
 				{
-					return new Lazy<Delegate>(() =>
-					{
-						var param = Expression.Parameter(x.Item1);
-						var prop = Expression.Property(param, x.Item2);
+					var param = Expression.Parameter(x.Item1);
+					var prop = Expression.Property(param, x.Item2);
 
-						return Expression.Lambda<Func<T, TDestination>>(prop, param).Compile();
-					});
-				}).Value;
-			}
+					return Expression.Lambda<Func<T, TDestination>>(prop, param).Compile();
+				});
+			}).Value;
 		}
 	}
 }
